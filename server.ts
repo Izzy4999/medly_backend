12222b0e1823542365768557f4bbd626d8bab0f1
Favour Israel Taiwo <princeisrael409@gmail.com>
2023-09-19 01:34:36 +0100

    updated server.js

--- server.ts
+++ server.ts
@@ -1,17 +1,6 @@
-import https from "https";
-import fs from "fs";
-
 import app from "./app";
 import env from "./utils/env";
 
-https
-  .createServer(
-    {
-      cert: fs.readFileSync("cert.crt"),
-      key: fs.readFileSync("key.key"),
-    },
-    app
-  )
-  .listen(env.PORT, () => {
-    console.log(`listening on ${env.PORT}`);
-  });
+app.listen(env.PORT, () => {
+  console.log(`listening on ${env.PORT}`);
+});
